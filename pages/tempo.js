284371ad251cd5bbd9ae8cbe30b11b34a017942e
Tiago Alexandre Mantovani <tiagoservodejesus@hotmail.com>
2021-02-02 07:46:21 -0300

    adiciona revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,26 +6,27 @@ function Tempo(props){
   return (
       <div>
           <div>{dynamicDateString} (dinamico)</div>
-          <div>{props.staticDateString} (estático - com delay)</div>
+          <div>{props.staticDateString} (estático)</div>
       </div>
   )
 }
 
 export async function getStaticProps() {
   console.log('> Passando pelo GetStaticProps()')
-  console.log('> Adicionando delay de 5 segundos')
+  //console.log('> Adicionando delay de 5 segundos')
 
-  await delay(5000);
+  //await delay(5000);
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
   return {
       props:{
         staticDateString
-      }
+      },
+      revalidate:1
   }
 }
 
-const delay = ms => new Promise(resolve => setTimeout(resolve,ms));
+//const delay = ms => new Promise(resolve => setTimeout(resolve,ms));
 
 export default Tempo;
